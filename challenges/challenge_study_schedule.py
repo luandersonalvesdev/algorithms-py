a154b674836b69045c183cf658100fe4b741d581
Luanderson Alves <luaoderson@gmail.com>
2023-09-20 19:53:27 -0300

    first exercise

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if target_time:
+        for period in permanence_period:
+            if all(isinstance(period, int) for period in period):
+                if period[0] <= target_time <= period[1]:
+                    count += 1
+            else:
+                return
+        return count
