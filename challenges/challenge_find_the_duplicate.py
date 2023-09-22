14f4d3b96cb41998b22a3f15ad4a109917060eb7
Luanderson Alves <luaoderson@gmail.com>
2023-09-21 21:16:16 -0300

    fifth exercise

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        all(isinstance(num, int) and num > 0 for num in nums)
+        and type(nums) == list
+        and len(nums) > 1
+    ):
+        nums.sort()
+        for i, num in enumerate(nums):
+            if (i + 1) == len(nums):
+                return False
+            if num == nums[i + 1]:
+                return num
+    return False
