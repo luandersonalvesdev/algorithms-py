187850b1965205be7fb1d6f99f94532936900c8e
Luanderson Alves <luaoderson@gmail.com>
2023-09-21 22:53:28 -0300

    refactor: fourth exercise, anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,34 +1,22 @@
-def bubble_sort(str_list):
-    n = len(str_list)
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if str_list[j] > str_list[j + 1]:
-                str_list[j], str_list[j + 1] = str_list[j + 1], str_list[j]
-
-
-def are_anagrams(first_string, second_string):
-    first_string = first_string.replace(" ", "").lower()
-    second_string = second_string.replace(" ", "").lower()
-
-    if len(first_string) != len(second_string):
-        return False
-
-    str1_list = list(first_string)
-    str2_list = list(second_string)
-
-    bubble_sort(str1_list)
-    bubble_sort(str2_list)
-
-    return str1_list == str2_list
+def quicksort(str_list):
+    if len(str_list) <= 1:
+        return str_list
+    else:
+        pivot = str_list[0]
+        less = [x for x in str_list[1:] if x <= pivot]
+        greater = [x for x in str_list[1:] if x > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
 
 
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    are_anagrams_result = are_anagrams(first_string, second_string)
+    if not first_string and not second_string:
+        return (first_string, second_string, False)
 
-    str1_sorted = list(first_string)
-    str2_sorted = list(second_string)
-    bubble_sort(str1_sorted)
-    bubble_sort(str2_sorted)
+    first_string = first_string.lower()
+    second_string = second_string.lower()
 
-    return ("".join(str1_sorted), "".join(str2_sorted), are_anagrams_result)
+    first_sorted = "".join(quicksort(list(first_string)))
+    second_sorted = "".join(quicksort(list(second_string)))
+
+    return (first_sorted, second_sorted, first_sorted == second_sorted)
