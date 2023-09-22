423b3fa59504f32fd26e36be1518b305f6a3313e
Luanderson Alves <luaoderson@gmail.com>
2023-09-21 21:29:36 -0300

    sixth exercise

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word:
+        high_index = len(word) - 1
+        low_index = 0
+
+        for letter in word:
+            if word[low_index] == word[high_index]:
+                low_index += 1
+                high_index -= 1
+            else:
+                return False
+
+        return True
+    else:
+        return False
